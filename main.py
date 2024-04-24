4ef3d2904b07601c59d3b17df28c1d0aa2d249bd
MateusTMagalhaes <159064680+MateusTMagalhaes@users.noreply.github.com>
2024-04-24 18:12:10 -0300

    main agora devolve a tree

--- main.py
+++ main.py
@@ -40,7 +40,34 @@ async def create_upload_file(file: UploadFile):
                 # Raise a new exception with the ID included in the message
                 raise HTTPException(status_code=400, detail=f"Erro na linha de ID {item['ID']}: {str(e)}")
 
-        return {'data': data}
+        def generate_json(df=df):
+            tree = {"children": []}
+            for index, row in df.iterrows():
+                pasta_origem = row["PastaOrigem"].strip()
+                pasta_destino = row["PastaDestino"].strip() if row["PastaDestino"] else None
+                pasta_backup = row["PastaBackup"].strip() if row["PastaBackup"] else None
+
+                if not any([pasta_origem, pasta_destino, pasta_backup]):
+                    continue 
+
+                node_origem = {"name": pasta_origem}
+
+                if pasta_destino:
+                    node_destino = {"name": pasta_destino}
+                    node_origem["children"] = [node_destino]
+
+                if pasta_backup:
+                    node_backup = {"name": pasta_backup}
+                    if "children" not in node_origem:
+                        node_origem["children"] = []
+                    node_origem["children"].append(node_backup)
+
+                tree["children"].append(node_origem)
+            return tree
+        
+        tree = generate_json()
+
+        print(tree)
+        return tree
     else:
         raise HTTPException(status_code=400, detail="Tipo de arquivo inválido")
-    #boot
